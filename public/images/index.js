4a0937c3c2177bcc5df093079f0be63df2cd28a5
PierceBalo <71556511+PierceBalo@users.noreply.github.com>
2023-03-20 00:05:43 -1200

    .

---
+++ public/images/index.js
@@ -0,0 +1,39 @@
+import close from "./close-outline.svg";
+import menu from "./menu-outline.svg";
+import imageSliderOne from "./imageSlider1.png";
+import imageSliderTwo from "./imageSlider2.png";
+import imageSliderThree from "./imageSlider3.png";
+import imageSliderFour from "./imageSlider4.png";
+import imageSlideFive from "./imageSlider5.png";
+import imageSliderSix from "./imageSlider6.png";
+import conceptOne from "./concept1.png";
+import conceptTwo from "./concept2.png";
+import publish from "./publications.png";
+import awardOne from "./award1.png";
+import awardTwo from "./award2.png";
+import awardThree from "./award3.png";
+import awardFour from "./award4.png";
+import awardFive from "./award5.png";
+import awardSix from "./award6.png";
+import office from "./home-office.png";
+
+export {
+  close,
+  menu,
+  imageSliderOne,
+  imageSliderTwo,
+  imageSliderThree,
+  imageSlideFive,
+  imageSliderFour,
+  imageSliderSix,
+  conceptOne,
+  conceptTwo,
+  publish,
+  awardOne,
+  awardTwo,
+  awardThree,
+  awardFour,
+  awardFive,
+  awardSix,
+  office,
+};
